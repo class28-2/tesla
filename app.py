fb063bdbe0084af0042b058d5008f98e876d8e82
simon <jelum85@gmail.com>
2022-05-25 16:42:46 -0400

    My first commit & program

---
+++ app.py
@@ -0,0 +1,5 @@
+print ('Tesla is best')
+echo "See below for latest changes"
+print ('God dey by my side solidly behing me')
+print ('what you starve dies, what you feed thrives/grows')
+print ('stave your vices, and feed your gifts, intelligence, oportunities and ambition')
