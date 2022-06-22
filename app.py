8397cf2893cb0bc6c6e20b275a6c87e88d493ec1
anitech85 <anitech85@gmail.com>
2022-06-21 22:17:17 -0400

    bug fixed, please review

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ echo "See below for latest changes"
 print ('God dey by my side solidly behing me')
 print ('what you starve dies, what you feed thrives/grows')
 print ('stave your vices, and feed your gifts, intelligence, oportunities and ambition')
+echo "bugs fixed, please review"
+
