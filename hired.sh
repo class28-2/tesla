7427718111036e985e8dbcc6ada0acb0f28d3c5f
simon <anitech85@gmail.com>
2022-06-24 21:02:59 -0400

    ta/master

--- hired.sh
+++ hired.sh
@@ -1,8 +1,5 @@
 echo "I am hired"
 echo "My salary is $200k+"
-<<<<<<< HEAD
 echo "Group 12 is the best. we are millionairs"
-
-=======
 echo "Group 12 is on fire!"
->>>>>>> d2c59909142a95973d9ed73f8b7ad48e61cb77d0
+echo "We need little more revision on git"
