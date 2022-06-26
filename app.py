21f7b3a0cafd809c4ae1d0917f44ed428b483bb3
anitech85 <105608451+anitech85@users.noreply.github.com>
2022-06-26 13:11:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ print ('Tesla is best')
 echo "See below for latest changes"
 print ('God dey by my side solidly behing me')
 print ('what you starve dies, what you feed thrives/grows')
-print ('stave your vices, and feed your gifts, intelligence, oportunities and ambitio
+print ('stave your vices, and feed your gifts, intelligence, oportunities and ambition)
+print ("Added this line to Jelum85's file as anitech85")
